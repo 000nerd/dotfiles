75b04c9cbbcffbb715e8ee9a39f9be124d6cd4cd
Obsa Aba-waji <oabawaji@gmail.com>
2017-10-02 21:42:18 -0400

    Adding brew packages to temp_brew to be merged with intstall/brew.sh

--- temp_brew.sh
+++ temp_brew.sh
@@ -221,3 +221,72 @@ mas install 948176063         # Boom 2 (1.6)
 mas install 1035350363        # Teeny Tokyo (1.2)
 mas install 872515009         # Pomodoro Timer (1.5)
 mas install 969418666         # ColorSnapper2 (1.3.1)
+
+# Install Homebrew
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap homebrew/versions
+brew tap homebrew/dupes
+brew tap Goles/battery
+brew tap homebrew/php
+brew update
+brew upgrade
+
+# Install packages
+brew install dockutil
+brew install htop
+brew install iftop
+brew install openssl
+brew install tig
+brew install composer
+brew install httpie
+brew install nmap
+brew install homebrew/php/php71
+brew install php71-xdebug
+brew install git
+brew install subversion
+brew install node
+brew install python3
+brew install thefuck
+brew install wget
+brew install yarn
+brew install zsh
+brew install zsh-completions
+brew install webkit2png
+brew install sqlmap
+brew install aircrack-ng
+brew install unrar
+brew install wifi-password
+
+# Wait a bit before moving on...
+sleep 1
+
+# ...and then.
+echo "Success! Basic brew packages are installed."
+
+# Cask
+brew tap caskroom/cask
+brew install brew-cask
+brew tap caskroom/versions
+
+# Install cask packages
+
+brew cask install --appdir="/Applications" alfred
+brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" slack
+brew cask install --appdir="/Applications" spotify
+brew cask install --appdir="/Applications" sublime-text
+brew cask install --appdir="/Applications" phpstorm
+brew cask install --appdir="/Applications" sequel-pro
+brew cask install --appdir="/Applications" filezilla
+brew cask install --appdir="/Applications" postman
+brew cask install --appdir="/Applications" teamviewer
+brew cask install --appdir="/Applications" spectacle
+brew cask install --appdir="/Applications" appcleaner
+brew cask install --appdir="/Applications" skype
+brew cask install --appdir="/Applications" the-unarchiver
+brew cask install --appdir="/Applications" iterm2
+brew cask install --appdir="/Applications" fork
+brew cask install iina
+
+# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
