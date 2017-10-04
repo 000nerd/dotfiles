23e287e26d042bc2406c884d12cb5d90f8761449
Obsa Aba-waji <oabawaji@gmail.com>
2017-10-03 23:56:27 -0400

    Updating macOS defaults. WIll be cleaning up next

--- install/macos.sh
+++ install/macos.sh
@@ -2,7 +2,7 @@
 
 ###############################################################################
 #                                  macOS                                      #
-############################################################################### 
+###############################################################################
 echo -e "\n\nChanging macOS settings"
 echo "=============================="
 # Close any open System Preferences panes, to prevent them from overriding
@@ -32,13 +32,13 @@ sudo pmset -a standbydelay 86400
 for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
     defaults write "${domain}" dontAutoLoad -array \
         "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
-        "/System/Library/CoreServices/Menu Extras/Volume.menu" \
         "/System/Library/CoreServices/Menu Extras/User.menu"
 done
 defaults write com.apple.systemuiserver menuExtras -array \
     "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
     "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
     "/System/Library/CoreServices/Menu Extras/Battery.menu" \
+    "/System/Library/CoreServices/Menu Extras/Volume.menu" \
     "/System/Library/CoreServices/Menu Extras/Clock.menu"
 
 # Set sidebar icon size to medium
@@ -48,9 +48,8 @@ defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
-# Display ASCII control characters using caret notation in standard text views
-# Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
-defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
 # Disable the crash reporter
 defaults write com.apple.CrashReporter DialogType -string "none"
@@ -79,6 +78,104 @@ defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
 # Enable Dark Mode
 defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+
+# ###############################################################################
+# # OS X Lockdown security tweaks
+# # read more: https://github.com/SummitRoute/osxlockdown
+# ###############################################################################
+# # Disable infrared receiver
+# sudo defaults write /Library/Preferences/com.apple.driver.AppleIRController DeviceEnabled -int 0
+
+# # Disable AirDrop
+# defaults write com.apple.NetworkBrowser DisableAirDrop -bool YES
+
+# # Set time and date automatically
+# sudo systemsetup setusingnetworktime on
+
+# # Disable remote apple events
+# sudo systemsetup -setremoteappleevents off
+
+# # Disable remote login
+# sudo systemsetup -f -setremotelogin off
+
+# # Disable screen sharing
+# launchctl unload -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
+
+# # Disable printer sharing
+# cupsctl --no-share-printers
+
+# # Disable wake on network access
+# sudo systemsetup -setwakeonnetworkaccess off
+
+# # Disable file sharing
+# launchctl unload -w /System/Library/LaunchDaemons/com.apple.AppleFileServer.plist && launchctl unload -w /System/Library/LaunchDaemons/com.apple.smbd.plist
+
+# # Disable remote management (must be root)
+# sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -stop
+
+# # Destroy file vault key when going into standby
+# sudo pmset -a destroyfvkeyonstandby 1
+
+# # Require an administrator password to access system-wide preferences
+# security authorizationdb read system.preferences > /tmp/system.preferences.plist &&/usr/libexec/PlistBuddy -c \"Set :shared false\" /tmp/system.preferences.plist && security authorizationdb write system.preferences < /tmp/system.preferences.plist
+
+# # Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
+# /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+# # Display ASCII control characters using caret notation in standard text views
+# # Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
+# # defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true
+
+# # Disable Resume system-wide
+# defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+
+# # Disable automatic termination of inactive apps
+# defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+
+# # Disable the crash reporter
+# defaults write com.apple.CrashReporter DialogType -string "none"
+
+# # Set Help Viewer windows to non-floating mode
+# defaults write com.apple.helpviewer DevMode -bool true
+
+# # Fix for the ancient UTF-8 bug in QuickLook (http://mths.be/bbo)
+# # Commented out, as this is known to cause problems in various Adobe apps :(
+# # See https://github.com/mathiasbynens/dotfiles/issues/237
+# #echo "0x08000100:0" > ~/.CFUserTextEncoding
+
+# # Reveal IP address, hostname, OS version, etc. when clicking the clock
+# # in the login window
+# sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# # Restart automatically if the computer freezes
+# sudo systemsetup -setrestartfreeze on
+
+# # Never go into computer sleep mode
+# sudo systemsetup -setcomputersleep Off > /dev/null
+
+# # Enable automatic software update checks
+# sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticCheckEnabled -bool TRUE
+# # Enable automatic software update installation
+# sudo defaults write /Library/Preferences/com.apple.commerce AutoUpdate -bool TRUE
+# # Check for software updates daily, not just once per week
+# defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# # Disable Notification Center and remove the menu bar icon
+# # launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
+
+# # Disable smart quotes as they’re annoying when typing code
+# defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# # Disable smart dashes as they’re annoying when typing code
+# defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# # Set a custom wallpaper image. `DefaultDesktop.jpg` is already a symlink, and
+# # all wallpapers are in `/Library/Desktop Pictures/`. The default is `Wave.jpg`.
+# #rm -rf ~/Library/Application Support/Dock/desktoppicture.db
+# #sudo rm -rf /System/Library/CoreServices/DefaultDesktop.jpg
+# #sudo ln -s /path/to/your/image /System/Library/CoreServices/DefaultDesktop.jpg
+
+
 
 ###############################################################################
 # SSD-specific tweaks                                                         #
@@ -136,12 +233,10 @@ defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
 # Follow the keyboard focus while zoomed in
 defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
-# Disable press-and-hold for keys in favor of key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
-# Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+# Automatically illuminate built-in MacBook keyboard in low light
+defaults write com.apple.BezelServices kDim -bool true
+# Turn off keyboard illumination when computer is not used for 5 minutes
+defaults write com.apple.BezelServices kDimTime -int 300
 
 # Set language and text formats
 defaults write NSGlobalDomain AppleLanguages -array "en"
@@ -215,6 +310,15 @@ defaults write com.apple.finder FXInfoPanesExpanded -dict \
 # Dock, Dashboard                                            #
 ###############################################################################
 
+# Enable highlight hover effect for the grid view of a stack (Dock)
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+
+# Set the icon size of Dock items to 48 pixels
+defaults write com.apple.dock tilesize -int 48
+
+# Set the Dock magnification off
+defaults write com.apple.dock magnification -bool false
+
 # Minimize windows into their application’s icon
 defaults write com.apple.dock minimize-to-application -bool true
 
@@ -235,6 +339,14 @@ defaults write com.apple.dock mru-spaces -bool false
 
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
+
+# Change minimize/maximize window effect
+defaults write com.apple.dock mineffect -string "genie"
+
+# Add iOS & Watch Simulator to Launchpad
+sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app" "/Applications/Simulator.app"
+sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator (Watch).app" "/Applications/Simulator (Watch).app"
+
 
 ###############################################################################
 # Safari & WebKit                                                             #
@@ -291,6 +403,9 @@ defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
 # Mail                                                                        #
 ###############################################################################
 
+# Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app
+defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" -string "@\\U21a9"
+
 # Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
 defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
@@ -298,9 +413,6 @@ defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
 defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes"
 defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date"
-
-# Disable automatic spell checking
-defaults write com.apple.mail SpellCheckingBehavior -string "NoSpellCheckingEnabled"
 
 ###############################################################################
 # Terminal & iTerm 2                                                          #
