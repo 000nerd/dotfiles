4ca8863ea2d235a07e86f453f27104c55c8c7156
Obsa Aba-waji <oabawaji@gmail.com>
2017-10-13 00:42:17 -0400

    Added apps that will be merged into install/brew in the future

--- temp_brew.sh
+++ temp_brew.sh
@@ -1,122 +1,150 @@
-brew "adns"
-brew "libyaml"
-brew "asciinema"
-brew "autoconf"
-brew "awscli"
-brew "mpfr"
-brew "cgal"
-brew "cmake"
-brew "consul"
-brew "coreutils"
-brew "curl", args: ["with-openssl"]
-brew "libassuan"
-brew "libksba"
-brew "pth"
-brew "dirmngr"
-brew "jpeg"
-brew "exa"
-brew "freexl"
-brew "fzf"
-brew "giflib"
-brew "json-c"
-brew "lzlib"
-brew "proj"
-brew "libgeotiff"
-brew "liblwgeom"
-brew "libspatialite"
-brew "gdal"
-brew "geoip"
-brew "gettext"
-brew "gpg-agent"
-brew "libusb-compat"
-brew "gnupg"
-brew "git-secret"
-brew "gnu-tar"
-brew "highlight"
-brew "htop"
-brew "libevent"
-brew "zlib"
-brew "httpd", args: ["with-mpm-event", "with-privileged-ports"]
-brew "httpie"
-brew "libtool"
-brew "imagemagick"
-brew "jq"
-brew "mercurial"
-brew "mongodb"
-brew "mysql"
-brew "nmap"
-brew "postgresql", restart_service: true
-brew "pgcli"
-brew "pinentry-mac"
-brew "sfcgal"
-brew "postgis"
-brew "qt"
-brew "redis"
-brew "rmtrash"
-brew "shellcheck"
-brew "ssh-copy-id"
-brew "swig"
-brew "tig"
-brew "tinc"
-brew "ttyrec"
-brew "ttygif"
-brew "wget"
-brew "z"
-brew install 'automake'
-brew install 'calc'
-brew install 'cmake'
-brew install 'coreutils'
-brew install 'cowsay'
-brew install 'ctags'
-brew install 'doxygen'
-brew install 'editorconfig'
-brew install 'fortune'
-brew install 'git'
-brew install 'git-flow'
-brew install 'grip'
-brew install 'iandrewt/tap/gpmdp-remote'
-brew install 'ical-buddy'
-brew install 'libksba'
-brew install 'mas'
-brew install 'mongodb'
-brew install 'mysql'
-brew install 'source-highlight'
-brew install 'tomanthony/brews/itermocil'
-brew install 'trash'
-brew install 'tree'
-brew install 'utf8proc'
-brew install 'watchman'
+"adns"
+"libyaml"
+"asciinema"
+"autoconf"
+"awscli"
+"mpfr"
+"cgal"
+"cmake"
+"consul"
+"coreutils"
+"curl", args: ["with-openssl"]
+"libassuan"
+"libksba"
+"pth"
+"dirmngr"
+"jpeg"
+"exa"
+"freexl"
+"fzf"
+"giflib"
+"json-c"
+"lzlib"
+"proj"
+"libgeotiff"
+"liblwgeom"
+"libspatialite"
+"gdal"
+"geoip"
+"gettext"
+"gpg-agent"
+"libusb-compat"
+"gnupg"
+"git-secret"
+"gnu-tar"
+"highlight"
+"htop"
+"libevent"
+"zlib"
+"httpd", args: ["with-mpm-event", "with-privileged-ports"]
+"httpie"
+"libtool"
+"imagemagick"
+"jq"
+"mercurial"
+"mongodb"
+"mysql"
+"nmap"
+"postgresql", restart_service: true
+"pgcli"
+"pinentry-mac"
+"sfcgal"
+"postgis"
+"qt"
+"redis"
+"rmtrash"
+"shellcheck"
+"ssh-copy-id"
+"swig"
+"tig"
+"tinc"
+"ttyrec"
+"ttygif"
+"wget"
+"z"
+'automake'
+'calc'
+'cmake'
+'coreutils'
+'cowsay'
+'ctags'
+'doxygen'
+'editorconfig'
+'fortune'
+'grip'
+'iandrewt/tap/gpmdp-remote'
+'ical-buddy'
+'libksba'
+'mas'
+'mongodb'
+'source-highlight'
+'tomanthony//itermocil'
+'trash'
+'tree'
+'utf8proc'
+'watchman'
+exiv2
+git-lfs
+git-flow
+git-extras
+imagemagick --with-webp
+lynx
+p7zip
+pigz
+pv
+rename
+rhino
+ssh-copy-id
+webkit2png
+zopfli
+pkg-config libffi
+pandoc
+dockutil
+iftop
+tig
+httpie
+nmap
+thefuck
+wget
+webkit2png
+unrar
+wifi-password
+moreutils
+findutils --with-default-names
+binutils
+diffutils
+gzip
+wdiff --with-gettext
 
-#brew install exiv2
-brew install git-lfs
-brew install git-flow
-brew install git-extras
-brew install imagemagick --with-webp
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install ssh-copy-id
-brew install webkit2png
-brew install zopfli
-brew install pkg-config libffi
-brew install pandoc
-brew install dockutil
-brew install htop
-brew install iftop
-brew install tig
-brew install httpie
-brew install nmap
-brew install thefuck
-brew install wget
-brew install webkit2png
-brew install unrar
-brew install wifi-password
-brew install moreutils
-brew install findutils --with-default-names
-brew install binutils
-brew install diffutils
-brew install gzip
-brew install wdiff --with-gettext
+# Apps look for cask or mas
+Etcher - Burn ISOs to USB drives
+Near Lock - Locks your computer when your iPhone goes out of range
+Knock - Knock on your iPhone to unlock Mac
+Textual Free - IRC client
+Hazel
+Rocket
+ForkLift
+Trip mode
+Rescue time
+Coconut battery
+Singlemizer
+Helium
+Pixelmator
+Space Gremlin
+Dash
+Magnet
+Mindnode
+Sketch
+WhatsApp Desktop
+Affinity Photo
+Twitter
+BlockBlock
+Soda Player
+1blocker
+Malwarebytes
+Spectacle
+Divvy
+Audacity
+MP3 Trimmer
+Karabiner
+GluePrint
