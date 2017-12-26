5be943334cda6260259fce4d048bafb5aa50bb79
Obsa Aba-waji <oabawaji@gmail.com>
2017-12-26 01:03:08 -0500

    Removing unused files

--- term.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Crispy Smooth font rendering
-defaults -currentHost write -globalDomain AppleFontSmoothing -int 2
-
-# update terminal
-tic -x ../iterm/xterm-256color-italic.terminfo
-tic -x ../iterm/tmux.terminfo
-tic -x ../iterm/tmux-256color.terminfo
