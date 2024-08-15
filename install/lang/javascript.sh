14548491557f69fe0d029bcd01f4496f86b06093
Obsa Aba-waji <oabawaji@gmail.com>
2024-08-14 23:18:04 -0400

    Update javascript.sh

--- install/lang/javascript.sh
+++ install/lang/javascript.sh
@@ -37,6 +37,7 @@ export NVM_DIR="$HOME/.nvm"
 
 # Install latest node and set it as default
 nvm install 18
+nvm install 20
 nvm install --lts
 nvm alias default 'lts/*'
 nvm use default # need to open new terminal/tab for changes to work
